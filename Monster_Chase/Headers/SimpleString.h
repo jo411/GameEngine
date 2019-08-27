ea827b4619e5320e0c4a300a841421fdca94f7b5
Josh Nelson <josh.nelson1@yahoo.com>
2019-08-27 17:37:53 -0600

    Added ecs skeleton

---
+++ Monster_Chase/Headers/SimpleString.h
@@ -0,0 +1,21 @@
+#pragma once
+class SimpleString
+{
+	char* string;
+	int length;
+
+	~SimpleString();
+	void nullTerminate();
+	void copyString(const SimpleString& other);
+public:
+	void append(char newChar);	
+	char charAt(int index) const;
+
+	SimpleString();
+	SimpleString(const SimpleString& other);
+	SimpleString(const char* string);
+	SimpleString& operator=(const SimpleString& other);
+	
+	int getLength();
+};
+
---
+++ Monster_Chase/Source/SimpleString.cpp
@@ -0,0 +1,82 @@
+#include "SimpleString.h"
+#include <malloc.h>
+
+
+
+SimpleString::SimpleString()
+{
+	length = 1;
+	string =(char*) malloc(sizeof(char) * length);
+	nullTerminate();
+}
+
+SimpleString& SimpleString::operator=(const SimpleString& rightSide)
+{	
+	if (this != &rightSide)
+	{
+		copyString(rightSide);
+	}
+
+	return *this;
+}
+
+SimpleString::SimpleString(const SimpleString& other)
+{
+	copyString(other);
+}
+
+SimpleString::SimpleString(const char* string)
+{
+	
+}
+
+SimpleString::~SimpleString()
+{
+	free(string);
+}
+
+void SimpleString::nullTerminate()
+{
+	string[length - 1] = '\0';
+}
+
+//Set this strings text to the others with a deep copy freeing the current memory if needed
+void SimpleString::copyString(const SimpleString& other)
+{
+	free(string);
+	string = NULL;
+	length = other.getLength + 1;
+	string = (char*)malloc(sizeof(char) * length);
+
+	for (int i = 0; i < length; i++)
+	{
+		string[i] = other.charAt(i);
+	}
+}
+
+int SimpleString::getLength()
+{
+	return length-1;//ignore null terminator
+}
+
+
+void SimpleString::append(char newChar)
+{
+	realloc(string, sizeof(char) * length + 1);
+	string[length-1] = newChar;
+	length++;
+	nullTerminate();
+}
+
+char SimpleString::charAt(int index) const
+{
+	if (index < length - 1)//ignore null terminator
+	{
+		return string[index];
+	}
+	else
+	{
+		return '\0';
+	}
+}
+
