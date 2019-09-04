e3e921f35c2f2fd03c966a6242bad4c4f365fee9
Josh Nelson <josh.nelson1@yahoo.com>
2019-09-04 14:26:12 -0600

    Build settings and removed precompiled headers

--- Engine/Engine/Component.h
+++ Engine/Engine/Component.h
@@ -4,19 +4,20 @@
 class GameObject;
 
 //The component class is attached to a game object and is updated when the gameobject revieves an update to modify its game objects behavior
+	class Component
+	{
+	protected:
+		GameObject* gameObject;//What G.O. is this component attached to
+		SimpleString* renderText;//A component may use this to set display text in draw() from its update() method. 
+	public:
+		virtual void update(UpdateParams* params) = 0;//defined in a derived class. Is called with its gameobject's update
+		virtual void draw(UpdateParams* params) = 0;//defined in a derived class. Is called with its gameobject's draw
+		virtual void onAddToObject() = 0;	//Defined in a derived class. Is called after a component is added to a gameobject
+		bool enabled = true; //will this component recieve or ignore updates. 
+		Component();
+		~Component();
+		void registerGameObject(GameObject* obj);//Stores this components attached game object
+	};
 
-class Component
-{
-protected:
-	GameObject* gameObject;//What G.O. is this component attached to
-	SimpleString* renderText;//A component may use this to set display text in draw() from its update() method. 
-public:
-	virtual void update(UpdateParams* params) = 0;//defined in a derived class. Is called with its gameobject's update
-	virtual void draw(UpdateParams* params) = 0;//defined in a derived class. Is called with its gameobject's draw
-	virtual void onAddToObject() = 0;	//Defined in a derived class. Is called after a component is added to a gameobject
-	bool enabled = true; //will this component recieve or ignore updates. 
-	Component();
-	~Component();
-	void registerGameObject(GameObject* obj);//Stores this components attached game object
-};
+
 
