8b4bc91d94fd48c77cd6512f50b89239fbdd0213
Josh Nelson <josh.nelson1@yahoo.com>
2019-12-13 18:37:14 -0700

    Implemented initialization and bit checking/setting functions for the bitarray

--- Engine/Engine/BitArray.cpp
+++ Engine/Engine/BitArray.cpp
@@ -1,1 +1,109 @@
 #include "BitArray.h"
+#include "MyMalloc.h"
+#include <intrin.h>
+#include<new>
+
+BitArray * BitArray::Create(size_t numBits, MyMalloc * heapManager)
+{
+	void* tmpPtr = heapManager->mm_malloc(sizeof(BitArray));
+		
+	BitArray* newBitArray = new (tmpPtr) BitArray(numBits, heapManager);
+
+	return newBitArray;
+}
+
+BitArray::BitArray(size_t numBits, MyMalloc * i_heapManager)
+{
+	bitCount = numBits;
+	heapManager = i_heapManager;
+
+	allignedWordCount = (size_t)ceil((double)numBits / WordSizeInBits());
+	
+	
+
+	bits = static_cast<size_t*>(heapManager->mm_malloc(allignedWordCount * sizeof(size_t)));
+	
+	ClearAll();
+
+	size_t index = 65;
+	SetBit(index);
+	ClearBit(index);
+	SetBit(66);
+	if (IsBitSet(66))
+	{
+		heapManager = nullptr;
+	}
+	else
+	{
+		heapManager = nullptr;
+	}
+
+
+}
+
+BitArray::~BitArray()
+{
+}
+
+void BitArray::ClearAll()
+{
+	for (unsigned int i = 0; i < allignedWordCount; i++)
+	{
+		bits[i] = 0;
+	}
+}
+
+void BitArray::SetAll()
+{
+	for (unsigned int i = 0; i < allignedWordCount; i++)
+	{
+		bits[i] = SIZE_MAX;
+	}
+}
+
+inline bool BitArray::IsBitSet(size_t index) const
+{
+	size_t wordIndex = getWordIndex(index);
+	size_t leftover = index - (wordIndex * WordSizeInBits());
+	return (bits[wordIndex] >> leftover) & 1ULL;
+	
+}
+
+inline bool BitArray::isBitClear(size_t index) const
+{
+	return !IsBitSet(index);
+}
+
+void BitArray::SetBit(size_t index)
+{
+	size_t wordIndex = getWordIndex(index);
+	size_t leftover = index - (wordIndex * WordSizeInBits());
+	bits[wordIndex] |= 1ULL << leftover;
+}
+
+void BitArray::ClearBit(size_t index)
+{
+	size_t wordIndex = getWordIndex(index);
+	size_t leftover = index - (wordIndex * WordSizeInBits());
+	bits[wordIndex] &= ~(1ULL << leftover);
+}
+
+bool BitArray::GetFirstClearBit(size_t & index) const
+{
+	return false;
+}
+
+bool BitArray::GetFirstSetBit(size_t & index) const
+{
+	return false;
+}
+
+bool BitArray::operator[](size_t index) const
+{
+	return false;
+}
+
+bool BitArray::isValid(size_t index)
+{
+	return (index >= 0 && index < bitCount);
+}
--- Engine/Engine/BitArray.h
+++ Engine/Engine/BitArray.h
@@ -1,8 +1,37 @@
 #pragma once
+#include "MyMalloc.h"
 class BitArray
 {
 public:
-	static BitArray* Create(size_t numBits);
+	static BitArray* Create(size_t numBits,MyMalloc* heapManager);
+	BitArray(size_t numBits, MyMalloc * heapManager);
+	~BitArray();
 
+	void ClearAll();
+	void SetAll();
+
+	inline bool IsBitSet(size_t index) const;
+	inline bool isBitClear(size_t index) const;
+
+	void SetBit(size_t index);
+	void ClearBit(size_t index);
+
+	bool GetFirstClearBit(size_t& index) const;
+	bool GetFirstSetBit(size_t & index) const;
+
+	bool operator[](size_t index)const;
+
+private:
+	MyMalloc* heapManager;
+	size_t bitCount;
+	size_t allignedWordCount;
+	inline size_t WordSizeInBits() const { return (sizeof(size_t) * 8); }
+	size_t* bits;//array of bits that this bit array tracks
+	//a bit array should be tied to a single memory manager. We dont want to allow copying
+	BitArray(const BitArray&) = delete;
+	BitArray& operator =(const BitArray&) = delete;
+
+	bool isValid(size_t index);
+	inline size_t getWordIndex(size_t bitIndex) const { return bitIndex / WordSizeInBits(); }
 };
 
