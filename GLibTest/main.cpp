64bedaad2f39a233e38999880b233a2788a2707e
Josh Nelson <josh.nelson1@yahoo.com>
2020-01-17 13:25:46 -0700

    More Rebuilding to link d3d11

--- GLibTest/main.cpp
+++ GLibTest/main.cpp
@@ -109,53 +109,7 @@ int WINAPI wWinMain( HINSTANCE i_hInstance, HINSTANCE i_hPrevInstance, LPWSTR i_
 
 }
 
-GLib::Sprites::Sprite * CreateSprite( const char * i_pFilename )
-{
-	assert( i_pFilename );
 
-	size_t sizeTextureFile = 0;
-
-	// Load the source file (texture data)
-	void * pTextureFile = LoadFile( i_pFilename, sizeTextureFile );
-
-	// Ask GLib to create a texture out of the data (assuming it was loaded successfully)
-	GLib::Texture * pTexture = pTextureFile ? GLib::CreateTexture( pTextureFile, sizeTextureFile ) : nullptr;
-
-	// exit if something didn't work
-	// probably need some debug logging in here!!!!
-	if( pTextureFile )
-		delete[] pTextureFile;
-
-	if( pTexture == nullptr )
-		return NULL;
-
-	unsigned int width = 0;
-	unsigned int height = 0;
-	unsigned int depth = 0;
-
-	// Get the dimensions of the texture. We'll use this to determine how big it is on screen
-	bool result = GLib::GetDimensions( pTexture, width, height, depth );
-	assert( result == true );
-	assert( ( width > 0 ) && ( height > 0 ) );
-
-	// Define the sprite edges
-	GLib::Sprites::SpriteEdges	Edges = { -float( width / 2.0f ), float( height ), float( width / 2.0f ), 0.0f };
-	GLib::Sprites::SpriteUVs	UVs = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } };
-	GLib::RGBA							Color = { 255, 255, 255, 255 };
-
-	// Create the sprite
-	GLib::Sprites::Sprite * pSprite = GLib::Sprites::CreateSprite( Edges, 0.1f, Color, UVs );
-	if( pSprite == nullptr )
-	{
-		GLib::Release( pTexture );
-		return nullptr;
-	}
-		
-	// Bind the texture to sprite
-	GLib::Sprites::SetTexture( *pSprite, *pTexture );
-
-	return pSprite;
-}
 
 void * LoadFile( const char * i_pFilename, size_t & o_sizeFile )
 {
