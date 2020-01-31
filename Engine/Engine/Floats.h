602efd3efd3c549b1a9f23daa12ef171617471b4
Josh Nelson <josh.nelson1@yahoo.com>
2020-01-31 12:34:17 -0700

    Is Zero and Relative Equality functions

---
+++ Engine/Engine/Floats.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <float.h>
+#include<cmath>
+class Floats
+{
+public:
+	static inline bool RelativeEpsilonEqual(float A, float B, float maxDiff = 0.0f, float maxRelDiff = FLT_EPSILON)
+	{
+		float diff = fabs(A - B);
+		if (diff <= maxDiff)
+		{
+			return true;
+		}
+
+		A = fabs(A);
+		B = fabs(B);
+		float max = (B > A) ? B : A;
+
+		if (diff <= max * maxRelDiff)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	static inline bool isZero(float A)
+	{
+		return RelativeEpsilonEqual(A, 0.0f, .000001f);
+	}
+
+};
+
