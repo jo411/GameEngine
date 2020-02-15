e22eaeba071bd7876815fe5ffa55c42ab5462fc8
Josh Nelson <josh.nelson1@yahoo.com>
2020-02-14 20:13:05 -0700

    Added Assignment notes to MonsterChase.cpp

--- Monster_Chase/Monster_Chase.cpp
+++ Monster_Chase/Monster_Chase.cpp
@@ -36,7 +36,11 @@
 //u0936149
 
 //-----------------------------------------------Assignment notes---------------------------------------------------------------
-//
+// Here is where all the code is located for this assignment. 
+//SmartPointer.h and WeakPointer.h inside the engine hold exactly what their names suggest 
+//RefCounter.h in the engine holds the reference counter
+//This file holds the tests I created. I turned off the display for this assignment. 
+//Person.h is a simple class I use in my tests. A person holds a name and age and can speak it to the output buffer.
 
 
 
