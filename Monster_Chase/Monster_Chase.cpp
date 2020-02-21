43944b9342904cf88b923b11e0063002bc4096b4
Josh Nelson <josh.nelson1@yahoo.com>
2020-02-21 12:37:47 -0700

    Removed smart pointer unit tests

--- Monster_Chase/Monster_Chase.cpp
+++ Monster_Chase/Monster_Chase.cpp
@@ -36,167 +36,17 @@
 //u0936149
 
 //-----------------------------------------------Assignment notes---------------------------------------------------------------
-// Here is where all the code is located for this assignment. 
-//SmartPointer.h and WeakPointer.h inside the engine hold exactly what their names suggest 
-//RefCounter.h in the engine holds the reference counter
-//This file holds the tests I created. I turned off the display for this assignment. 
-//Person.h is a simple class I use in my tests. A person holds a name and age and can speak it to the output buffer.
+//
+//
+//
+//
+//
 
 
 
-
-void testSmartPointersOnly()
-{
-	SmartPointer<Person> p(new Person("Josh", 23));
-	p->Display();
-	{
-		SmartPointer<Person> q = p;
-		q->Display();
-	
-
-		SmartPointer<Person> r;
-		r = p;
-		r->Display();
-		// Destructor of r will be called here..
-			// Destructor of q will be called here..
-	}
-	p->Display();
-	// Destructor of p will be called here 
-	// and person pointer will be deleted
-}
-void testSmartPointerComparison()
-{
-	for (int i = 0; i < 10; i++)
-	{
-		SmartPointer<Person> p(new Person("Josh", 23));
-		SmartPointer<Person> q(new Person("Josh2", 46));
-		if (p < q)
-		{
-			assert(p < q);
-		}
-		else
-		{
-			assert(p > q);
-		}
-
-		if (p == p)
-		{
-			assert(p == p);
-		}
-		else
-		{
-			assert(p != p);
-		}
-		if (p <= p)
-		{
-			assert(p <= p);
-		}
-		else
-		{
-			assert(p >= p);
-		}
-		if ((q < p))
-		{
-			assert((q < p));
-		}
-		else
-		{
-			assert((q > p));
-		}
-	}
-	
-}
-
-bool testSmartNull()
-{
-	SmartPointer<Person> p;	
-	{
-		SmartPointer<Person> q = p;
-		if (q)
-		{
-			return false;
-		}
-		// Destructor of q will be called here..
-
-		SmartPointer<Person> r;
-		SmartPointer<Person> s;
-		r = p;
-		r = SmartPointer<Person>(new Person("Josh", 23));
-		if (!r)
-		{
-			return false;
-		}
-		r->Display();
-		// Destructor of r will be called here..
-	}
-	if (p)
-	{
-		return false;
-	}
-	return true;
-}
-
-bool TestWeakGet()
-{
-	SmartPointer<Person> p(new Person("Promoted pointer", 23));
-	WeakPointer<Person> observer(p);
-
-	assert(p);
-
-	assert(observer.Aquire());
-
-	SmartPointer<Person> promoted = observer.Aquire();
-
-	promoted->Display();
-	return true;
-}
-
-bool TestWeakDoesntDelete()
-{
-	SmartPointer<Person> p(new Person("Promoted pointer", 23));
-
-	{
-		WeakPointer<Person> observer(p);
-		if (!observer.Aquire())
-		{
-			return false;
-		}
-	}
-	if (!p)
-	{
-		return false;
-	}
-	return true;
-}
-
-bool TestSmartAndWeakNull()
-{
-	WeakPointer<Person> wp1;
-	{
-		SmartPointer<Person> p(new Person("Good pointer", 23));
-		WeakPointer<Person> wp2(p);
-		wp1 = wp2;
-	}
-	
-	if (wp1.IsAlive())
-	{
-		return false;
-	}	
-	return(true);
-}
 
 int WINAPI wWinMain(HINSTANCE i_hInstance, HINSTANCE i_hPrevInstance, LPWSTR i_lpCmdLine, int i_nCmdShow)
-{
-	{
-		testSmartPointersOnly();
-		testSmartPointerComparison();
-		assert(testSmartNull());
-		assert(TestWeakGet());
-		assert(TestWeakDoesntDelete());
-		assert(TestSmartAndWeakNull());
-	}	
-	_CrtDumpMemoryLeaks();
-	return 1;
+{	
 	{
 		// IMPORTANT: first we need to initialize GLib		
 		bool bSuccess = GLib::Initialize(i_hInstance, i_nCmdShow, "GLibTest", -1, 800, 600);
