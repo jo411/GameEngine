796a3342d11d369ab70b88d33ebafa1fb73e19e6
Josh Nelson <josh.nelson1@yahoo.com>
2020-03-20 14:59:36 -0600

    Initial vector4 class

--- Engine/Engine/Math/Vector4.cpp
+++ Engine/Engine/Math/Vector4.cpp
@@ -1,1 +1,46 @@
 #include "Vector4.h"
+
+Vector4::Vector4():px(0.0f), py(0.0f),pz(0.0f),pw(0.0f)
+{	
+}
+
+Vector4::Vector4(float i_x, float i_y, float i_z, float i_w) : px(i_x), py(i_y), pz(i_z), pw(i_w)
+{
+}
+
+Vector4 Vector4::operator=(const Vector4 & v)
+{
+	if (this != &v) {
+		px = v.X();
+		py = v.Y();
+		pz = v.Z();
+		pw = v.W();
+	}
+	return *this;
+}
+
+const Vector4 Vector4::operator*(const float & scalar) const
+{
+	return Vector4(px*scalar, py*scalar, pz*scalar, pw*scalar);
+}
+
+const Vector4 Vector4::operator+(const Vector4 & v) const
+{
+	return Vector4
+	(	px + v.X(),
+		py + v.Y(),
+		pz + v.Z(),
+		pw + v.W()
+	);
+}
+
+const Vector4 Vector4::Scale(const Vector4 & A, const Vector4 & B) const
+{
+	return Vector4();
+}
+
+std::ostream & operator<<(std::ostream & os, const Vector4 & v4)
+{
+	os << "(" << v4.X() << "," << v4.Y() << "," << v4.Z() << "," << v4.W() << ")";
+	return os;
+}
--- Engine/Engine/Math/Vector4.h
+++ Engine/Engine/Math/Vector4.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <iostream>
 class Vector4
 {
+public:
+	Vector4();
+	Vector4(float i_x, float i_y, float i_z, float i_w = 0.0f);
+	Vector4 operator= (const Vector4& v);
+
+	const Vector4 operator* (const float &scalar) const;
+	const Vector4 operator+ (const Vector4 &v) const;
+	const Vector4 Scale(const Vector4 &A, const Vector4 &B) const;
+	
+	float X() const { return px; };
+	float Y() const { return py; };
+	float Z() const { return pz; };
+	float W() const { return pw; };
+	
+	friend std::ostream& operator<<(std::ostream& os, const Vector4& v4);
+private:
+	float px;
+	float py;
+	float pz;
+	float pw;
 };
 
