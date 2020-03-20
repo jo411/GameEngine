e75527fe0387715b1ca3811b90fbebb19d257c46
Josh Nelson <josh.nelson1@yahoo.com>
2020-03-20 15:46:46 -0600

    Wrote Matrix4 skeleton.

--- Engine/Engine/Math/Matrix4.cpp
+++ Engine/Engine/Math/Matrix4.cpp
@@ -1,1 +1,64 @@
 #include "Matrix4.h"
+
+Matrix4::Matrix4()
+{
+}
+
+Matrix4::Matrix4(float i_11, float i_12, float i_13, float i_14, float i_21, float i_22, float i_23, float i_24, float i_31, float i_32, float i_33, float i_34, float i_41, float i_42, float i_43, float i_44)
+{
+}
+
+Matrix4::Matrix4(const Vector4 & v1, const Vector4 & v2, const Vector4 & v3, const Vector4 & v4)
+{
+}
+
+float Matrix4::operator()(unsigned row, unsigned col) const
+{
+	return 0.0f;
+}
+
+Matrix4 Matrix4::createTranslation(float x, float y, float z)
+{
+	return Matrix4();
+}
+
+Matrix4 Matrix4::createScale(float x, float y, float z)
+{
+	return Matrix4();
+}
+
+Matrix4 Matrix4::createRotationX(float angle)
+{
+	return Matrix4();
+}
+
+Matrix4 Matrix4::createRotationY(float angle)
+{
+	return Matrix4();
+}
+
+Matrix4 Matrix4::createRotationZ(float angle)
+{
+	return Matrix4();
+}
+
+Matrix4 Matrix4::getTranspose()
+{
+	return Matrix4();
+}
+
+// Allows us to use V = M * V (i.e. column vector)
+inline Vector4 operator*(const Matrix4 & i_mtx, const Vector4 & i_vec)
+{
+	return Vector4();
+}
+// Allows us to use V = V * M; (i.e. row vector)
+inline Vector4 operator*(const Vector4 & i_vec, const Matrix4 & i_mtx)
+{
+	return Vector4();
+}
+
+const Matrix4 operator*(const Matrix4 & RHS, const Matrix4 & LHS)
+{
+	return Matrix4();
+}
--- Engine/Engine/Math/Matrix4.h
+++ Engine/Engine/Math/Matrix4.h
@@ -1,6 +1,34 @@
 #pragma once
+#include "Vector4.h"
 class Matrix4
 {
+public:
+	Matrix4();
+	Matrix4(float i_11, float i_12, float i_13, float i_14,
+			float i_21, float i_22, float i_23, float i_24,
+			float i_31, float i_32, float i_33, float i_34,
+			float i_41, float i_42, float i_43, float i_44);
+	Matrix4(const Vector4& v1, const Vector4& v2, const Vector4& v3, const Vector4& v4);
+	
+	
+	float operator() (unsigned row, unsigned col) const;	
 
+	static Matrix4 createTranslation(float x, float y, float z);
+	static Matrix4 createScale(float x, float y, float z);
+	static Matrix4 createRotationX(float angle);
+	static Matrix4 createRotationY(float angle);
+	static Matrix4 createRotationZ(float angle);
+		
+	Matrix4 getTranspose();
+
+private:
+
+	float m[4][4];
+	
 };
+// Allows us to use V = M * V (i.e. column vector)
+inline Vector4 operator*(const Matrix4 & i_mtx, const Vector4 & i_vec);
+// Allows us to use V = V * M; (i.e. row vector)
+inline Vector4 operator*(const Vector4 & i_vec, const Matrix4 & i_mtx);
 
+const Matrix4 operator* (const Matrix4 &RHS, const Matrix4 &LHS);
