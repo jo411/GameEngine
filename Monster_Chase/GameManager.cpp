621233fb3d95e50a65397b57cb13285cc42bc6e8
Josh Nelson <josh.nelson1@yahoo.com>
2020-05-07 18:28:55 -0600

    Created a gamemanager component

--- Monster_Chase/GameManager.cpp
+++ Monster_Chase/GameManager.cpp
@@ -1,1 +1,25 @@
 #include "GameManager.h"
+const std::string GameManager::tag = "gamecontroller";
+GameManager::GameManager()
+{
+}
+
+GameManager::~GameManager()
+{
+}
+
+void GameManager::update(UpdateParams * params)
+{
+}
+
+void GameManager::draw(UpdateParams * params)
+{
+}
+
+void GameManager::onAddToObject()
+{
+}
+
+void GameManager::Serialize(json & j)
+{
+}
--- Monster_Chase/GameManager.h
+++ Monster_Chase/GameManager.h
@@ -1,5 +1,22 @@
 #pragma once
-class GameManager
+#include "GameObject.h"
+class GameManager : public Component
 {
+public:
+	GameManager();
+	~GameManager();
+
+	void update(UpdateParams* params);
+	void draw(UpdateParams* params);
+	void onAddToObject();
+	
+	void Serialize(json & j);	
+
+	static const std::string tag;
+	virtual const std::string& getTag() { return tag; }
+	static const std::string tag;
+	virtual const std::string& getTag() { return tag; }
+private:
+
 };
 
